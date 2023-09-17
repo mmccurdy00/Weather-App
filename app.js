103643b1d7c78199d8c6c52ff730c80f881e8dde
mmccurdy00 <142942959+mmccurdy00@users.noreply.github.com>
2023-09-17 09:52:49 -0500

    Got JS working to display correct terms

--- app.js
+++ app.js
@@ -1,59 +1,87 @@
-let now = new Date();
-let currentDay = document.querySelector("#current-day");
-let currentTime = document.querySelector("#current-time");
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-currentDay.innerHTML = `${day}`;
-currentTime.innerHTML = `${now.getHours()}:${now.getMinutes()}`;
-
-function search(event) {
-  event.preventDefault();
-  let h1 = document.querySelector("h1");
-  let searchInput = document.querySelector("#search");
-  if (searchInput.value) {
-    h1.innerHTML = `~${searchInput.value}~`;
-  } else {
-    h1.innerHTML = null;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
-  return searchInput.value;
-}
-let form = document.querySelector("form");
-form.addEventListener("submit", myFunction);
-
-function degreesF(event) {
-  let newOne = document.querySelector("#temperature");
-  newOne.innerHTML = "101°";
-}
-let fahren = document.querySelector("#f");
-fahren.addEventListener("click", degreesF);
-
-function degreesC(event) {
-  let newTwo = document.querySelector("#temperature");
-  newTwo.innerHTML = "38°";
-}
-let cel = document.querySelector("#c");
-cel.addEventListener("click", degreesC);
-
-function myFunction(event) {
-  event.preventDefault();
-  let x = document.querySelector("#search").value;
-  function displayWeather(response) {
-    let weather = document.querySelector("#temperature");
-    let temperature = Math.round(response.data.main.temp);
-    let description = response.data.weather[0].description;
-    weather.innerHTML = `${temperature}°C <br /> ${description}`;
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = `~${response.data.name}~`;
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
-  let searchInput = document.querySelector("#search-input").value;
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=197ef3a642b76eef90e131866f74a0a0&units=metric`;
-  axios.get(url).then(displayWeather);
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  let celsiusTemperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
+}
+
+function search(city) {
+  let apiKey = "a0f3f839feb6c8a05faec943ed4tb06o";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Tokyo");
